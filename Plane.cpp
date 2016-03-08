853aae9796b778571a8276134f6a36ae9052a87b
aquira246 <munchkin9955@yahoo.com>
2016-03-07 21:03:44 -0800

    planes now compile

--- Plane.cpp
+++ Plane.cpp
@@ -10,12 +10,14 @@ Plane::Plane() {
     normal = Eigen::Vector3f(0,0,-1);
     radius = 1.0f;
 }
-Plane::Plane(Eigen::Vector3f c, Eigen::Vector3f n, float r);
+
+Plane::Plane(Eigen::Vector3f c, Eigen::Vector3f n, float r) {
     SetMaterialByNum(rand() % NUM_MATS);
     center = c;
     normal = n;
     radius = r;
 }
+
 Plane::~Plane(){
 
 }
@@ -23,6 +25,7 @@ Plane::~Plane(){
 float Plane::checkHit(Eigen::Vector3f eye, Eigen::Vector3f dir) {
     
     float t = -1;
+
     // assuming vectors are all normalized
     float denom = dot(normal, dir);
     
@@ -31,10 +34,10 @@ float Plane::checkHit(Eigen::Vector3f eye, Eigen::Vector3f dir) {
         t = dot(p0l0, normal) / denom;
     }
 
-    if (t < 0) return -1
+    if (t < 0) return -1;
 
-    Vec3f p = eye + dir * t;
-    Vec3f v = p - center;
+    Eigen::Vector3f p = eye + dir * t;
+    Eigen::Vector3f v = p - center;
     float d2 = dot(v, v);
 
     if (d2 <= radius*radius) return t;
